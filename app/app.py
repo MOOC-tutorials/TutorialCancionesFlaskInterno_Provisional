e224bed8606a7347d9da73ffbedcfa2344128aae
usuariocoursera1 <jp.reyes39@uniandes.edu.co>
2021-03-11 19:45:45 -0500

    Creación de esquemas de clases con marshmallow-SQLAlchemy

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
-from modelos.modelos import db, Usuario, Album, Medio, Cancion, albumes_canciones
+from modelos.modelos import db, Usuario, Album, Medio, Cancion, albumes_canciones, CancionSchema, AlbumSchema, UsuarioSchema
 from flask import Flask
 
 app = Flask(__name__)
 db.init_app(app)
 
+cancion_schema = CancionSchema()
+album_schema = AlbumSchema()
+usuario_schema = UsuarioSchema()
+
 with app.app_context():
     db.create_all()
-    
--- app/modelos/modelos.py
+++ app/modelos/modelos.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields
 import enum
 
 class Medio(enum.Enum):
@@ -34,3 +36,28 @@ class Usuario(db.Model):
     nombre = db.Column(db.String(50))
     contrasena = db.Column(db.String(50))
     albumes = db.relationship('Album', cascade='all, delete, delete-orphan')
+
+class EnumADiccionario(fields.Field):
+    def _serialize(self, value, attr, obj, **kwargs):
+        if value is None:
+            return None
+        return {"llave": value.name, "valor": value.value}
+
+class CancionSchema(SQLAlchemyAutoSchema):
+    class Meta:
+         model = Cancion
+         include_relationships = True
+         load_instance = True
+
+class AlbumSchema(SQLAlchemyAutoSchema):
+    medio = EnumADiccionario(attribute=("medio"))
+    class Meta:
+         model = Album
+         include_relationships = True
+         load_instance = True
+
+class UsuarioSchema(SQLAlchemyAutoSchema):
+    class Meta:
+         model = Usuario
+         include_relationships = True
+         load_instance = True
