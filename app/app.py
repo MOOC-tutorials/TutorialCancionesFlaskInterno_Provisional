15f8eb76ccb814a471125232902951f5b41d693b
usuariocoursera1 <jp.reyes39@uniandes.edu.co>
2021-04-27 15:53:06 -0500

    Añadida vista de signin y token de seguridad

--- app/app.py
+++ app/app.py
@@ -2,20 +2,26 @@ from modelos.modelos import db
 from flask import Flask
 from flask_restful import Api
 from vistas.vistas import *
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
-db.init_app(app)
-api = Api(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+
+db.init_app(app)
+api = Api(app)
+jwt = JWTManager(app)
+
+
 
 api.add_resource(VistaAlbums, '/usuario/<int:id_usuario>/albumes')
 api.add_resource(VistaAlbum, '/album/<int:id_album>')
-api.add_resource(VistaLogin, '/login')
+api.add_resource(VistaSignIn, '/signin')
+api.add_resource(VistaLogIn, '/login')
 api.add_resource(VistaUsuario, '/usuario/<int:id_usuario>')
 api.add_resource(VistaCanciones, '/canciones')
 api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
-
 
 with app.app_context():
     db.create_all()
--- app/vistas/vistas.py
+++ app/vistas/vistas.py
@@ -2,6 +2,7 @@ from flask import request
 from modelos.modelos import db, Album, Medio, AlbumSchema, Usuario, UsuarioSchema, Cancion, CancionSchema
 from flask_restful import Resource, reqparse
 from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 
 album_schema = AlbumSchema()
 usuario_schema = UsuarioSchema()
@@ -9,6 +10,7 @@ cancion_schema = CancionSchema()
 
 class VistaAlbums(Resource):
 
+    @jwt_required()
     def post(self, id_usuario):
         nuevo_album = Album(titulo=request.json["titulo"], anio=request.json["anio"], descripcion=request.json["descripcion"], medio=request.json["medio"])
         usuario = Usuario.query.get_or_404(id_usuario)
@@ -21,20 +23,24 @@ class VistaAlbums(Resource):
         
         return album_schema.dump(nuevo_album)
 
+    @jwt_required()
     def get(self, id_usuario):
         usuario = Usuario.query.get_or_404(id_usuario)
         return [album_schema.dump(al) for al in usuario.albumes]
 
 class VistaAlbum(Resource):
 
+    @jwt_required()
     def get(self, id_album):
         return album_schema.dump(Album.query.get_or_404(id_album))
 
+    @jwt_required()
     def post(self, id_album):
         nuevo_album = Album(titulo=request.json["titulo"], anio=request.json["anio"], descripcion=request.json["descripcion"], medio=request.json["medio"])
         usuario = Usuario.query.get_or_404(id_usuario)
         usuario.albumes.append(nuevo_album)
 
+    @jwt_required()
     def put(self, id_album):
         album = Album.query.get_or_404(id_album)
         album.titulo = request.json.get("titulo",album.titulo)
@@ -44,6 +50,7 @@ class VistaAlbum(Resource):
         db.session.commit()
         return album_schema.dump(album)
 
+    @jwt_required()
     def delete(self, id_album):
         album = Album.query.get_or_404(id_album)
         db.session.delete(album)
@@ -51,29 +58,51 @@ class VistaAlbum(Resource):
         return '',204
 
 
-class VistaLogin(Resource):
 
-    def post(self):
-        nuevo_usuario = Usuario(nombre=request.json["nombre"], contrasena=request.json["contrasena"])
-        db.session.add(nuevo_usuario)
-        db.session.commit()
 
 class VistaUsuario(Resource):
 
+    @jwt_required()
     def get(self, id_usuario):
         return usuario_schema.dump(Usuario.query.get_or_404(id_usuario))
 
+    @jwt_required()
     def delete(self, id_usuario):
         usuario = Usuario.query.get_or_404(id_usuario)
         db.session.delete(usuario)
         db.session.commit()
         return '',204
 
+    @jwt_required()
     def put(self, id_usuario):
         usuario = Usuario.query.get_or_404(id_usuario)
         usuario.contrasena = request.json.get("contrasena",usuario.contrasena)
         db.session.commit()
         return usuario_schema.dump(usuario)
+
+class VistaSignIn(Resource):
+
+    def post(self):
+        nuevo_usuario = Usuario(nombre=request.json["nombre"], contrasena=request.json["contrasena"])
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+        access_token = create_access_token(identity = request.json["nombre"])
+        return {"mensaje":"usuario creado exitosamente", "token de acceso":access_token}
+
+class VistaLogIn(Resource):
+
+    def post(self):
+        username = request.json.get("nombre")
+        contrasena = request.json.get("contrasena")
+        usuario = Usuario.query.filter_by(username = username).first()
+        if not usuario:
+            return {"mensaje":"El usuario no está registrado"}
+        else:
+            if contrasena != usuario.contrasena:
+                return {"mensaje":"contraseña incorrecta"}
+            else:
+                access_token = create_access_token(identity = username)
+                return {"mensaje":"login exitoso", "token de acceso":access_token}
 
 class VistaCanciones(Resource):
 
